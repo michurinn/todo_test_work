6f5065a7aa347a864dd852857bf88d370bc96899
michurinn <michurinnn@gmail.com>
2024-05-21 21:19:18 +0300

    feat: UI has been described

--- lib/src/todo_list_feature/presentation/pages/sample_item_list_view.dart
+++ lib/src/todo_list_feature/presentation/pages/sample_item_list_view.dart
@@ -1,57 +1,126 @@
-import 'dart:math';
-
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:todo_testwork/src/settings/settings_view.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_testwork/src/app/app_constants/item_colors.dart';
+import 'package:todo_testwork/src/todo_list_feature/di/todo_list_scope.dart';
 import 'package:todo_testwork/src/todo_list_feature/domain/entity/todo_entity.dart';
 import 'package:todo_testwork/src/todo_list_feature/presentation/state_managers/todo_boc/todo_bloc.dart';
+import 'package:todo_testwork/src/todo_list_feature/presentation/widgets/show_description_dialog.dart';
 
 /// Displays a list of SampleItems.
 class SampleItemListView extends StatelessWidget {
   const SampleItemListView({
     super.key,
-    this.items = const [SampleItem(1), SampleItem(2), SampleItem(3)],
   });
 
   static const routeName = '/';
 
-  final List<SampleItem> items;
-
   @override
   Widget build(BuildContext context) {
-    final bloc = context.watch<TodoBloc>();
+    final bloc = context.read<TodoListScope>().todoListBloc;
+    if (kDebugMode) {
+      print('Print test');
+    }
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Sample Items'),
-      ),
-      body: ListView.builder(
-        restorationId: 'sampleItemListView',
-        itemCount: items.length,
-        itemBuilder: (context, index) {
-          final item = items[index];
-          return ListTile(
-              title: Text('SampleItem ${item.id}'),
-              leading: const CircleAvatar(
-                // Display the Flutter Logo image asset.
-                foregroundImage: AssetImage('assets/images/flutter_logo.png'),
-              ),
-              onTap: () {
-                bloc.add(
-                  AddEvent(
-                    todo: TodoEntity(
-                        id: Random.secure().nextInt(9999),
-                        title: 'New random item'),
+      appBar: AppBar(title: const Text('Sample Items'), actions: [
+        BlocBuilder<TodoBloc, TodoState>(
+          bloc: bloc,
+          builder: (context, state) => switch (state) {
+            Loading() => const SizedBox.shrink(),
+            Loaded(:final selectedItemId) => selectedItemId != null
+                ? Wrap(children: [
+                    IconButton(
+                      onPressed: () async {
+                        final title = await showDescriptionDialog(context);
+                        bloc.add(
+                          UpdateItemTitle(
+                              id: selectedItemId, title: title ?? ''),
+                        );
+                      },
+                      icon: const Icon(Icons.edit),
+                    ),
+                    IconButton(
+                      onPressed: () => bloc.add(
+                        DoneEvent(
+                          id: selectedItemId,
+                        ),
+                      ),
+                      icon: const Icon(Icons.done_all_rounded),
+                    ),
+                    IconButton(
+                      onPressed: () => bloc.add(
+                        DeleteCheckedEvent(),
+                      ),
+                      icon: const Icon(Icons.delete_forever_outlined),
+                    )
+                  ])
+                : const SizedBox.shrink(),
+            Error() => const SizedBox.shrink(),
+          },
+        )
+      ]),
+      body: BlocBuilder<TodoBloc, TodoState>(
+          bloc: bloc,
+          builder: (context, state) {
+            return switch (state) {
+              Loading() => const Center(
+                  child: CircularProgressIndicator.adaptive(),
+                ),
+              Error(:final message) => Center(
+                  child: Text(message),
+                ),
+              Loaded(:final todoList) => ReorderableListView.builder(
+                  itemCount: todoList.length,
+                  itemBuilder: (context, index) {
+                    final item = todoList[index];
+                    return DecoratedBox(
+                      key: ValueKey(item.id),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(16),
+                          color: item.id == state.selectedItemId
+                              ? ItemColors.Amber.color.withOpacity(.2)
+                              : null),
+                      child: CheckboxListTile(
+                        value: item.isCompleted,
+                        title: Text('${item.title} ${item.index} ${item.id}'),
+                        onChanged: (value) {
+                          if (value != null) {
+                            bloc.add(
+                              SelectItem(id: item.id),
+                            );
+                          }
+                        },
+                      ),
+                    );
+                  },
+                  onReorder: (oldIndex, newIndex) => bloc.add(
+                    SwapEvent(
+                      firsItemId: todoList.getItemIdByElementIndex(oldIndex),
+                      secondItemId: todoList.getItemIdByElementIndex(newIndex),
+                    ),
                   ),
-                );
-              });
+                ),
+            };
+          }),
+      floatingActionButton: FloatingActionButton.large(
+        child: const Icon(Icons.plus_one),
+        onPressed: () async {
+          final title = await showDescriptionDialog(context);
+          if (title != null) {
+            bloc.add(
+              AddEvent(
+                title: title,
+              ),
+            );
+          }
         },
       ),
     );
   }
 }
 
-class SampleItem {
-  final int id;
-
-  const SampleItem(this.id);
+extension on List<TodoEntity> {
+  int getItemIdByElementIndex(int elementIndex) {
+    return this[elementIndex.clamp(0, length - 1)].id;
+  }
 }
---
+++ lib/src/todo_list_feature/presentation/widgets/show_description_dialog.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:todo_testwork/src/app/app_constants/app_constants.dart';
+import 'package:todo_testwork/src/app/extensions/string_hardcoded.dart';
+
+class _TodoDialog extends StatefulWidget {
+  const _TodoDialog({super.key, this.initialText, required this.onEditing});
+  final String? initialText;
+  final ValueChanged<String> onEditing;
+  @override
+  State<_TodoDialog> createState() => _TodoDialogState();
+}
+
+class _TodoDialogState extends State<_TodoDialog> {
+  late final TextEditingController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = TextEditingController();
+    if (widget.initialText != null) {
+      controller.text = widget.initialText!;
+    }
+    controller.addListener(
+      () => widget.onEditing(
+        controller.text,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      autofocus: true,
+      minLines: 4,
+      maxLines: 4,
+      decoration: InputDecoration(
+          hintText: 'Think about the important things'.hardcoded),
+      maxLength: AppConstants.maxTitleLenght,
+    );
+  }
+}
+
+Future<String?> showDescriptionDialog(
+  BuildContext context,
+  [String? initialText]
+) async {
+  String? storedText;
+  String onChanged(String text) => storedText = text;
+  final result = await showAdaptiveDialog<String?>(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: Text('Add a new todo'.hardcoded),
+        content: _TodoDialog(
+          initialText: initialText,
+          onEditing: onChanged,
+        ),
+        actions: [
+          TextButton(
+            child: Text('Cancel'.hardcoded),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          TextButton(
+            child: Text('Add'.hardcoded),
+            onPressed: () {
+              Navigator.pop(context, storedText);
+            },
+          ),
+        ],
+      );
+    },
+  );
+  return result;
+}
